a69b11da751a73cd09143345b52d287464ea5e44
Hamed <HamedMosavi@Yahoo.com>
2014-12-17 13:07:48 +0330

    / Updated Command factory by adding a registry of commands that have been created already to minimize number of command creation.

--- Src/ProductionMan/ProductionMan.Desktop/CommandFactory.cs
+++ Src/ProductionMan/ProductionMan.Desktop/CommandFactory.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
 using ProductionMan.Desktop.Commands;
 using ProductionMan.Domain.Security;
 
@@ -9,15 +11,58 @@ namespace ProductionMan.Desktop
     public class CommandFactory
     {
 
+        private enum CommandKey
+        {
+            Exit,
+            Login,
+            RetryLogin
+        }
+
+        private readonly Dictionary<CommandKey, ICommand> _commands;
+
+
+        public CommandFactory()
+        {
+            _commands = new Dictionary<CommandKey, ICommand>();
+        }
+
+
+        private ICommand CreateCommand<TCommandType>(CommandKey commandKey) where TCommandType : ICommand
+        {
+            if (!_commands.ContainsKey(commandKey))
+            {
+                _commands.Add(commandKey, Activator.CreateInstance<TCommandType>());
+            }
+
+            return _commands[commandKey];
+        }
+
+
+        private ICommand CreateCommand(CommandKey commandKey, Type type, object[] parameters)
+        {
+            if (!_commands.ContainsKey(commandKey))
+            {
+                _commands.Add(commandKey, (ICommand)Activator.CreateInstance(type, parameters));
+            }
+
+            return _commands[commandKey];
+        }
+
+
         internal ICommand CreateLoginCommand(User user)
         {
-            return new LoginCommand(user);
+            return CreateCommand(CommandKey.Login, typeof (LoginCommand), new object[] {user});
         }
 
 
         internal ICommand CreateExitCommand()
         {
-            return new ExitCommand();
+            return CreateCommand<ExitCommand>(CommandKey.Exit);
+        }
+
+        public ICommand CreateRetryLoginCommand(User user)
+        {
+            return CreateCommand(CommandKey.RetryLogin, typeof(LoginRetryCommand), new object[] { user });
         }
     }
 }
---
+++ Src/ProductionMan/ProductionMan.Desktop/Commands/LoginRetryCommand.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Input;
+using ProductionMan.Domain.Security;
+
+namespace ProductionMan.Desktop.Commands
+{
+    public class LoginRetryCommand : ICommand
+    {
+
+        private readonly User _user;
+
+
+        public LoginRetryCommand(User user)
+        {
+            _user = user;
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            return _user != null && _user.LoginStatus != User.LoginStates.SigningIn;
+        }
+
+
+        public event System.EventHandler CanExecuteChanged;
+
+
+        public void Execute(object parameter)
+        {
+            if (_user != null)
+            {
+                _user.RequestRetry();
+            }
+        }
+    }
+}
